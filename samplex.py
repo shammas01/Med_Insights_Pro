52d6d2c6d5af24bdda11662ce883e6f6e4356715
shammas01 <shammasmhd0@gmail.com>
2023-09-07 11:51:37 +0530

    edited

--- samplex.py
+++ samplex.py
@@ -10,17 +10,30 @@
 #     queryset = MyModel.objects.all()
 #     serializer_class = MyModelSerializer
 
+
+
+
+
+
 # samples with python language >>>>>>>>>>>>>>>>>>>
 
+# string method
 message = "Hello, world!"
 length = len(message)
 uppercase_message = message.upper()
 print(uppercase_message)
 print(type(uppercase_message))
 
-
+#try and 
 try:
     result = 10 / 0
     print(result)
 except ZeroDivisionError:
     print("Cannot divide by zero")
+
+# file handiling  
+with open("data.txt", "r") as file:
+    content = file.read()
+with open("output.txt", "w") as file:
+    file.write("Hello, file!")
+    
